843dea58eafaa330571815da3e684fe5987800c5
dchayto <dlchaytor@outlook.com>
2026-01-04 15:22:48 -0500

    due_hw_interface.cpp updates didn't commit for some reason

--- pi/src/control/src/due_hw_interface.cpp
+++ pi/src/control/src/due_hw_interface.cpp
@@ -28,7 +28,7 @@
 
 //include "UARTmsgs.hpp"
 
-const char[] UART_PORT = "/dev/ttyACM0";
+const char UART_PORT[]= "/dev/ttyACM0";
 static int serialPort; // move to private member var once node implemented
 
 // obvs move buffers to private member variables once ros node implemented
@@ -85,12 +85,11 @@ void openPort()	{
 }
 
 void readUART(char * buf, size_t bufsize)	{
-	int n = read(serialPort, buf, sizeof(encBuff));
+	read(serialPort, buf, bufsize);
 }
 
 void writeUART(char * msg, size_t msgsize)	{
 	// v probably need to send/recieve these messages as delimited strings
-	//write(serialPort, wheelIn, sizeof(UARTmsgs::WheelSpeed));
 	write(serialPort, msg, msgsize);
 }
 
@@ -99,14 +98,17 @@ int main(int argc, char** argv)	{
 
 	// NOTE: arduino seems to need ~150ms setup time more than pi to init port
 	std::this_thread::sleep_for(std::chrono::milliseconds(150)); 
-	char j {};
-	for (char i = 65; i <= 122; ++i)	{
-		std::cout << "Sending message: " << i << std::endl;
-		writeUART(&i, 1);
-		std::this_thread::sleep_for(std::chrono::milliseconds(250)); 
-		readUART(&j, 1)
-		std::cout << "Receiving message: " << j << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(250)); 
+	UARTmsgs::WheelSpeed k { 0, 0, 0, 0 };
+	UARTmsgs::WheelSpeed j { 0, 0, 0, 0 };
+	for (int i = -128; i <= 127; ++i)	{
+		k.FR = k.FL = k.BR = k.BL = i;
+		k.encodeMsg();
+		std::cout << "Sending message: " << k.msg << std::endl;
+		writeUART(k.msg, UARTmsgs::MSG_SIZE);
+		std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
+		readUART(j.msg, UARTmsgs::MSG_SIZE)
+		std::cout << "Receiving message: " << j.msg << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
 	}
 
 	closePort();
